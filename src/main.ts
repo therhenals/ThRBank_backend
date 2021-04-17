e9e28a40fdcad636a883dd1b077f4e95f2b37f1e
TheRhenals <therhenalsl@gmail.com>
2021-04-17 15:06:15 -0500

    Init swagger OPENAPI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Initial swagger
+  const options = new DocumentBuilder()
+    .setTitle('ThRBank API')
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  // Validators
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
